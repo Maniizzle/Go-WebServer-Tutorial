4cd4cf9ed421ad889f827199ac0f0ccd8f28db7d
Olamide Onakoya <31809831+Maniizzle@users.noreply.github.com>
2023-10-24 06:27:05 +0100

    Update reference

--- 05-MultiRoute-ServeMux-WebServer/server/main.go
+++ 05-MultiRoute-ServeMux-WebServer/server/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"GO-WebServer-Tutorial/02-REST-GET-WebServer/countryCapitals"
+	"GO-WebServer-Tutorial/05-MultiRoute-ServeMux-WebServer/countryCapitals"
 	"fmt"
 	"io"
 	"log"
